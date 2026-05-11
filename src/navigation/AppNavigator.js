7a58c2aee5e72e24f631261a56bbf34339101233
Jakkula-Ajay <jakkulaajay199@gmail.com>
2026-05-11 13:15:48 +0530

    Updated app flow and fixed logout functionality

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,54 +1,68 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator }
-from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthContext }
+from '../context/AuthContext';
 import LoginScreen from '../screens/LoginScreen';
 import OtpScreen from '../screens/OtpScreen';
 import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
 import CartScreen from '../screens/CartScreen';
 import CheckoutScreen from '../screens/CheckoutScreen';
 import OrderSuccessScreen from '../screens/OrderSuccessScreen';
-import DetailsScreen from '../screens/DetailsScreen';
-const Stack = createNativeStackNavigator();
+const Stack =
+  createNativeStackNavigator();
 const AppNavigator = () => {
+  const { isLoggedIn } =
+    useContext(AuthContext);
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: true,
+        }}
+      >
+        {!isLoggedIn ? (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+            />
+            <Stack.Screen
+              name="Otp"
+              component={OtpScreen}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+            />
 
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-        />
+            <Stack.Screen
+              name="Details"
+              component={DetailsScreen}
+            />
 
-        <Stack.Screen
-          name="Otp"
-          component={OtpScreen}
-        />
+            <Stack.Screen
+              name="Cart"
+              component={CartScreen}
+            />
 
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
+            <Stack.Screen
+              name="Checkout"
+              component={CheckoutScreen}
+            />
 
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-        />
+            <Stack.Screen
+              name="Success"
+              component={OrderSuccessScreen}
+            />
 
-        <Stack.Screen
-          name="Cart"
-          component={CartScreen}
-        />
+          </>
 
-        <Stack.Screen
-          name="Checkout"
-          component={CheckoutScreen}
-        />
-
-        <Stack.Screen
-          name="Success"
-          component={OrderSuccessScreen}
-        />
+        )}
 
       </Stack.Navigator>
 
